33e898c4295b5ed04b8184c90bdc344e97a4462e
TARISO <toni.ariso@gmail.com>
2020-09-01 06:54:22 +0200

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/Vue Drum Machine/' : '/',
+    process.env.NODE_ENV === 'production' ? '/Vue-Drum-Machine/' : '/',
 };
