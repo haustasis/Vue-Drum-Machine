f30e3f495382a9bf8541dbd6b823751240ce2bb5
TARISO <toni.ariso@gmail.com>
2020-09-01 05:13:58 +0200

    clips

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container>
+      <b-row>
+        <b-col md="6" offset-md="3">
+          <b-card class="mt-4">
+            <b-row class="d-flex justify-content-between mx-2">
+              <b-form-checkbox v-model="isOn" switch size="lg"
+                >Power</b-form-checkbox
+              >
+              <div class="border rounded-lg px-4 py-2">
+                {{ lastPlayed }}
+              </div>
+            </b-row>
+            <b-row class="mx-2 mt-4">
+              <label for="volume">Volume</label>
+              <b-form-input v-model="value" type="range" min="0" max="5" />
+            </b-row>
+          </b-card>
+          <b-card class="my-4">
+            <b-row class="justify-content-center">
+              <div
+                :class="[
+                  'sound-button border border-warning rounded-lg m-1',
+                  { 'button-disabled': !isOn },
+                ]"
+                v-for="clip in clips"
+                :key="clip.name"
+                @click="playClip(clip)"
+              >
+                {{ clip.name }}
+              </div>
+            </b-row>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import clips from './clips';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  data() {
+    return {
+      clips,
+      isOn: true,
+      lastPlayed: 'No Sound',
+    };
+  },
+  methods: {
+    playClip({ name, url }) {
+      if (this.isOn) {
+        let s = new Audio(url);
+        s.play();
+        this.lastPlayed = name;
+      }
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.sound-button {
+  width: 160px;
+  height: 160px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.button-disabled {
+  border: gray;
 }
 </style>
